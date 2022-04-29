16f468e9ebf90fea243445330b1a96641eb44043
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-04-29 13:52:02 -0400

    added modals to login and nav

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
-import Navigation from "./components/Navigation";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,16 +12,12 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return isLoaded && (
+  return (
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
-
         <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path={"/signup"}>
+          <Route path="/signup">
             <SignupFormPage />
           </Route>
         </Switch>
