6a5219a4f5f6f1d8c725c7cc480ccd0252124331
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-04-30 23:17:43 -0400

    added some hard-code elements to front end. will replace with JSX as I build back end

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -9,11 +9,11 @@ const csrfProtection = csrf({ cookie: true });
 
 router.get('/', (async(req,res)=>{
     const locations = await Location.findAll({}) //locations is an array of all location items
-    const locationName = locations.map(location=> location.name) //array of all location names: Full 4 Bed 3 Bath home
-    const locationAddress = locations.map(location=> location.address) //array of all location address: 9320 talbot dr.
+    const locationName = locations.map(location => location.name) //array of all location names: Full 4 Bed 3 Bath home
+    const locationAddress = locations.map(location => location.address) //array of all location address: 9320 talbot dr.
     const locationState = locations.map(location => location.state) //array of all location states: PA,FL
-    const locationCity = locations.map(location=> location.city)
-    const locationCounty = locations.map(location=> location.country)
+    const locationCity = locations.map(location => location.city)
+    const locationCounty = locations.map(location => location.country)
     const price = locations.map(location => location.price)
     const locationOwnerId = locations.map(location => location.userId)
     let usersId = [];
