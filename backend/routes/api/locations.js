6a887564c7bd2203765142304944a19a52cdc609
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-05-04 13:10:13 -0400

    added new route location to a file

--- backend/routes/api/locations.js
+++ backend/routes/api/locations.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {User,Location, Image} = require('/Users/anthonybronca/Desktop/w14-solo/backend/db/models');
+const {User,Location, Image} = require('../../db/models');
 
 
 
