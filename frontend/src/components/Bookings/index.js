35c11874e073c623890ff0a962625242cc2b77c0
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-05-27 03:37:20 -0400

    began component, added store, and added route for user bookings

--- frontend/src/components/Bookings/index.js
+++ frontend/src/components/Bookings/index.js
@@ -2,17 +2,38 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import { getAllBookings } from "../../store/bookings";
+import Errors from "../Errors";
 
-function Bookings () {
+function Bookings() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const user = useParams()
+    const idParams = useParams()
+    const userAuth = useSelector(state => state.session.user.id)
+    const bookings = useSelector(state => state.bookings)
+    console.log(bookings, "this is bookingsss")
 
-    return (
-        <>
-        <h1>test</h1>
+    useEffect(()=> {
+        dispatch(getAllBookings(userAuth))
+    }, [dispatch])
+
+    if (idParams.id != userAuth) {
+        return (
+            <Errors />
+        )
+    } else {
+        return (
+            <>
+            <div>
+                <h1 className='user-listings-header'>Your Listings</h1>
+            </div>
+            <div id="main-container">
+                <div id="locations-layout">
+
+                </div>
+            </div>
         </>
         )
+    }
 }
 
 export default Bookings
--- frontend/src/store/bookings.js
+++ frontend/src/store/bookings.js
@@ -15,6 +15,7 @@ export const getAllBookings = (userId) =>  async (dispatch)  => {
     const response = await csrfFetch(`/api/user/${userId}/bookings`)
     const bookings = await response.json()
     dispatch(addBookings(bookings));
+    // console.log(bookings, "this is bookings from the thunk")
     return bookings;
 }
 
@@ -25,13 +26,11 @@ let newState;
 switch(action.type){
         case GET_BOOKINGS:
             newState = {...state};
-            const bookingData = action.payload;
-            newState.bookingAndLocations = bookingData;
-
-            // bookingData.forEach(el => newState.locations = el)
-            const imageItems = action.payload;
-            // imageData.forEach(el=> newState.imageItems = el)
-            newState.imageData = imageItems
+            const bookings = action.payload.Bookings[0]
+            newState['Bookings'] = bookings
+            const bookingLocation = bookings.Location
+            newState['Locations'] = bookingLocation
+            newState['Images'] = bookingLocation.Images[0]
             return newState
     //   action.payload.forEach(booking => {
     //     newState[booking.id] = booking
